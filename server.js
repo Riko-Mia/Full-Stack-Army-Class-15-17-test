31b04f3488a7af409a83c24d0bb5a3755cca7fea
Riko Mia <rikomia722@gmail.com>
2023-07-17 23:39:15 +0600

    Express Middleware understanding

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+/** video 15 start
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
@@ -27,4 +28,41 @@ app.get('/name', (_req, res) => {
 app.listen(4000, () => {
     console.log('Server is listing on port 4000')
 
+}) 
+Video 15 end */
+
+/** Video 16 start 
+const express = require('express')
+const morgen = require('morgan')
+
+const app = express()
+
+// Simple logger (request tester) ############### ########## 
+const myLogger = (req,res,next) => {
+    console.log(`${req.url} - ${req.method} - ${new Date().toISOString()}` )
+    next()
+}
+
+
+//app.use(myLogger) // Custom Logger globally connected. ************* Function don't invoking or call. 
+app.use([
+    morgen('dev')
+])
+
+
+
+// this is router model ****************** ################ @@@@@@@@@@@@@@@@@@ %%%%%%%%%%% 
+app.get('/goodby',(req, res, next) => {
+   // console.log(`${req.url} - ${req.method} - ${new Date().toISOString()}`) // logger test
+    res.json({name : 'Riko ahmed'}) 
 })
+app.get('/house', (req, res, next) => {
+    res.json({home : 'sweet home'}) 
+})
+
+
+app.listen(4000, () => {
+    console.log('Application running 4000')
+})
+*/
+
